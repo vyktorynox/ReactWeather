3a5536ccc003ef6467dd98afa5c270ee12e72716
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-08 19:25:27 -0400

    Adding custom styles support and fixing version foundation sites to allways download ver. 6.2.0

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -33,7 +33,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weahter"/>
+                <input type="search" placeholder="Search weather by city"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
