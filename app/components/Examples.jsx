bc6b5cb52a6e66c5e83676c933a7332195267b9e
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-08 16:37:52 -0400

    Adding styles to main and examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=verdun'>Verdun, FR</Link>
+        </li>
+        <li>
+          <Link to='/?location=petting'>Petting, DE</Link>
+        </li>
+        <li>
+          <Link to='/?location=fukking'>Tittmoning, DE</Link>
+        </li>
+      </ol>
     </div>
   )
 };
