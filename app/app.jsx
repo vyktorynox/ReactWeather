0a4d3a5915accb1a8e396721e15ae1643ebc8e5b
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2018-01-22 21:32:43 -0400

    Upgrading jquery to v3

--- app/app.jsx
+++ app/app.jsx
@@ -8,7 +8,8 @@ var About = require('About');
 var Examples = require('Examples');
 
 //load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation-float.min.css');
 $(document).foundation();
 
 //App css
